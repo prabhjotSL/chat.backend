4d9b7250c3a08aeb992f9d3e633b4dc1c9c50108
Armin Krauss <armin.krauss@utoronto.ca>
2013-11-15 14:31:14 -0500

    Adding lodash for underscore utils

--- app.js
+++ app.js
@@ -1,56 +1,59 @@
 var express = require('express');
 var app = express();
+var _ = require('lodash');
 app.use(express.bodyParser()); // this is needed to parse the body of requests like POST and PUT
 
-var quotes = [
-  { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
-  { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
-  { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
-  { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
+var workers = [
+  { _id : 1, first_name : "colin", last_name : "mccann", password : "chat", role_name : "councelor", assigned_community : "snathing" },
+  { _id : 2, first_name : "Armin", last_name : "Krauss", password : "chat", role_name : "volunteer", assigned_community : "snathing" }
 ];
 
 
 app.get('/', function(req, res) {
   res.type('text/plain');
-  res.send('this is a test api spitting out quotes. Use /quotes to see all /quote/random to get a random quote and /quote/[id] to get a defined quote');
+  res.send('This will be the API used by the CHAT Android app for data syncing.\n Add /workers to retrieve all worker records. More to come');
 });
 
-app.get('/quotes', function(req, res) {
-  res.json(quotes);
+app.get('/workers', function(req, res) {
+  res.json(workers);
 });
 
-app.get('/quote/random', function(req, res) {
-  var id = Math.floor(Math.random() * quotes.length);
-  var q = quotes[id];
-  res.json(q);
-});
+app.get('/worker/:id', function(req, res) {
+  var worker = _.find(workers, function (w) {
+    return w._id === parseInt(req.params.id, 10);
+  });
 
-app.get('/quote/:id', function(req, res) {
-  if(quotes.length <= req.params.id || req.params.id < 0) {
+  if (worker) {
+    res.json(worker);
+  } else {
     res.statusCode = 404;
-    return res.send('Error 404: No quote found');
+    return res.send('Error 404: No worker record found found');
   }
+  // if(workers.length <= req.params.id || req.params.id < 0) {
+  //   res.statusCode = 404;
+  //   return res.send('Error 404: No worker record found found');
+  // }
 
-  var q = quotes[req.params.id];
-  res.json(q);
+  // var q = workers[req.params.id];
+  // res.json(q);
 });
 
-app.post('/quote', function(req, res) {
-  // console.log(req);
-  if(!req.body.hasOwnProperty('author') || 
-     !req.body.hasOwnProperty('text')) {
-    res.statusCode = 400;
-    return res.send('Error 400: Post syntax incorrect.');
-  }
+// app.post('/quote', function(req, res) {
+//   // console.log(req);
+//   if(!req.body.hasOwnProperty('author') || 
+//      !req.body.hasOwnProperty('text')) {
+//     res.statusCode = 400;
+//     return res.send('Error 400: Post syntax incorrect.');
+//   }
 
-  var newQuote = {
-    author : req.body.author,
-    text : req.body.text
-  };
-  // console.log(newQuote);
-  quotes.push(newQuote);
-  // console.log(quotes);
-  res.json(true);
-});
+//   var newQuote = {
+//     author : req.body.author,
+//     text : req.body.text
+//   };
+//   // console.log(newQuote);
+//   quotes.push(newQuote);
+//   // console.log(quotes);
+//   res.json(true);
+// });
 
 app.listen(process.env.PORT || 8000);
