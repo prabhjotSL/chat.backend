68b5c7da72177b870b2b16461caf77d30bcea954
Armin Krauss <armin.krauss@utoronto.ca>
2014-02-25 23:06:27 -0500

    Updated visits model to reflect fields found in Android code

--- models/visit.js
+++ models/visit.js
@@ -1,7 +1,15 @@
 var mongoose = require('mongoose');
 var Visit = new mongoose.Schema({
   _id : Number,
-  name : String
+  worker_id : Number,
+  role: String,
+  date: Date,
+  hh_id: Number,
+  lat: Number,
+  lon: Number,
+  start_time: Date,
+  end_time: Date,
+  type: String
 }, { versionKey: false });
 // Hiding __v - version key http://stackoverflow.com/questions/13699784/mongoose-v-property-hide
 module.exports = mongoose.model('Visit', Visit);
