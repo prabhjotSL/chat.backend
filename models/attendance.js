51870bfefe7eb725c919fddda88f95ec0f730a97
Armin Krauss <armin.krauss@utoronto.ca>
2014-03-16 22:24:48 -0400

    Ensuring that attendance is not pluralized

--- models/attendance.js
+++ models/attendance.js
@@ -3,6 +3,6 @@ var Attendance = new mongoose.Schema({
   _id : Number,
   visit_id : Number,
   client_id : Number
-}, { versionKey: false });
+}, { versionKey: false, collection: 'attendance' });
 // Hiding __v - version key http://stackoverflow.com/questions/13699784/mongoose-v-property-hide
 module.exports = mongoose.model('Attendance', Attendance);
