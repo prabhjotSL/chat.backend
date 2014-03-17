8e4cb2a30fcd52f2d469b2bd3f503a2b5d77873f
Armin Krauss <armin.krauss@utoronto.ca>
2014-03-17 14:57:15 -0400

    Adding attendance and resources_accessed as arrays to the visit model

--- models/visit.js
+++ models/visit.js
@@ -8,7 +8,9 @@ var Visit = new mongoose.Schema({
   lon: Number,
   start_time: Date,
   end_time: Date,
-  type: String
+  type: String,
+  attendance: Array,
+  resources_accessed: Array
 }, { versionKey: false });
 // Hiding __v - version key http://stackoverflow.com/questions/13699784/mongoose-v-property-hide
 module.exports = mongoose.model('Visit', Visit);
